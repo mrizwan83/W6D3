9d598f0387255370479de67aa21297b502d85293
App Academy Student <email@email.com>
2022-06-29 15:35:29 -0400

    completion of routes_and_controllers

---
+++ routes_and_controllers/app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true 
+end
